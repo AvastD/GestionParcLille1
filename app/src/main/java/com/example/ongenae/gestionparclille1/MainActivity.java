ac8c793ccc23568c2835a63640c84736cfbb0144
Dorian Ongenae <dorian.ongenae@etudiant.univ-lille1.fr>
2017-12-07 17:12:18 +0100

    création début main activity avec floating button

--- app/src/main/java/com/example/ongenae/gestionparclille1/MainActivity.java
+++ app/src/main/java/com/example/ongenae/gestionparclille1/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ongenae.gestionparclille1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * On click pour le bouton flottant permettant d'accéder à l'activity d'ajou de problème
+     * @param view
+     */
+    public void go_to_add_issue(View view) {
+        // lancer add_issue_activity
     }
 }
